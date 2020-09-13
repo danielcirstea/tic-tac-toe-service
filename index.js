bc2c0a0804b12c0d3a0d19333e7cebc6c5caf4bf
unknown <daniel.cirstea96@gmail.com>
2020-09-13 16:33:15 +0300

    created boilerplate for API

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const config = require('./lib/common/config')();
+const api = require('./lib');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const info = require('./package.json');
+
+app.use(helmet());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.use('/api', api);
+app.use('*', (req, res) => {
+    res.status(200).json({ name: info.name, version: info.version, status: 'online' });
+});
+
+http.createServer(app).listen(config.app.port, () => {
+    console.log(`Listening on ${config.app.port}.`);
+});
