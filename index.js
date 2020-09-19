99563a2704286a287d8335ec9f399704e62774ce
unknown <daniel.cirstea96@gmail.com>
2020-09-19 20:48:40 +0300

    added mongo db read/write functionality and built API

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ const api = require('./lib');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const info = require('./package.json');
+const cors = require('cors');
 
+app.use(cors());
 app.use(helmet());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use('/api', api);
+if (config.db.mongo.uri) {
+    app.use('/api', api);
+}
+
 app.use('*', (req, res) => {
-    res.status(200).json({ name: info.name, version: info.version, status: 'online' });
+    res.status(200).json({name: info.name, version: info.version, status: 'online'});
 });
 
 http.createServer(app).listen(config.app.port, () => {
